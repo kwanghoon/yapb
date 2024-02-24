5c9b54330ff01e276ba9c35318f6ffeba6eb3a21
Kwanghoon Choi <lazyswamp@gmail.com>
2024-02-24 22:25:31 +0900

    change counting into listing

--- src/parserlib/analyze/AnalyzeActions.hs
+++ src/parserlib/analyze/AnalyzeActions.hs
@@ -6,7 +6,7 @@ import TokenInterface ( TokenInterface )
 import Terminal ( terminalToTokenSymbol )
 import Control.Monad.RWS (MonadState(state))
 import Data.Maybe
-import qualified Data.Map as Map
+-- import qualified Data.Map as Map
 import qualified Data.Bifunctor
 import Test.Hspec (xcontext)
 
@@ -21,18 +21,16 @@ analyzeActions logs =
 printNoLn :: Show a => a -> IO ()
 printNoLn = putStr . show
 
-printResult :: Map.Map State (Map.Map CandidateForest Integer) -> IO ()
-printResult map =   -- [ (State, [ (CandidateForest, Integer) ] ) ]
-    mapM_ pr [ (state, candForestCount)
-                | (state, candForestCountMap) <- Map.toList map,
-                  candForestCount <- Map.toList candForestCountMap ]
+printResult :: [(State, CandidateForest)] -> IO ()
+printResult stateCandidate =   -- [ (State, [ (CandidateForest, Integer) ] ) ]
+    mapM_ pr stateCandidate
     where
-        pr (state, (candForest, count)) = -- state candidate count
+        pr (state, candForest) = -- state candidate count
             do printNoLn state
                putStr " "
                putStr (concatMap (prSymbolWithSp . unleaf) candForest)
                -- putStr " "
-               printNoLn count
+               -- printNoLn count
                putStrLn ""
 
         unleaf (Leaf x) = x
@@ -41,31 +39,33 @@ printResult map =   -- [ (State, [ (CandidateForest, Integer) ] ) ]
         prSymbolWithSp sym = show sym ++ " "
 
 
-collect :: TokenInterface token => ActionLogs token -> Map.Map State ( Map.Map CandidateForest Integer )
+collect :: TokenInterface token => ActionLogs token ->[(State, CandidateForest)]
 collect logs =
     let initState = 0
-        map = onLogs initState logs Map.empty
+        map = onLogs initState logs []
     in  map
 
 
 onLogs :: TokenInterface token => State -- ^ 
   -> [ActionLog token] -- ^ 
-  -> Map.Map State ( Map.Map CandidateForest Integer ) -- ^ state |-> { candidate1 |-> n1, ..., candidatek |-> nk }
-  -> Map.Map State ( Map.Map CandidateForest Integer )
+  -> [(State, CandidateForest)] -- ^ state |-> { candidate1 |-> n1, ..., candidatek |-> nk }
+  -> [(State, CandidateForest)]
 onLogs state [] map = map
 onLogs state logs map =
     let candidate = initRepReduce state logs
         (maybeNextState, nextLogs) = getNextState logs
 
-        foundMap = Map.fromList [ (candidate,1) ]
+        -- foundMap = Map.fromList [ (candidate,1) ]
 
-        f newMap oldMap =
-          let (v,c) = head (Map.toList newMap) in
-            Map.insertWith g v c oldMap
-        g nc oc = oc + nc
+        -- f newMap oldMap =
+        --   let (v,c) = head (Map.toList newMap) in
+        --     Map.insertWith g v c oldMap
+        -- g nc oc = oc + nc
 
+        -- newMap = if null candidate then map
+        --          else Map.insertWith f state foundMap map
         newMap = if null candidate then map
-                 else Map.insertWith f state foundMap map
+                 else map ++ [(state, candidate)]
     in
         -- trace ("onLogs: " ++ show state ++
         --        "        " ++ showActionLog (head logs) ++
